9a0e78e9e2d5ae1516e6ce0281d1804c55059680
sluzerpp <bistboy@list.ru>
2023-03-19 11:56:38 +0300

    feat: add routes

--- react-component/src/App.tsx
+++ react-component/src/App.tsx
@@ -1,12 +1,17 @@
 import './App.css';
 import React from 'react';
-import SearchBar from './assets/components/SearchBar';
+import { Route, Routes } from 'react-router-dom';
+import AboutPage from './pages/AboutPage';
+import Page404 from './pages/Page404';
+import MainPage from './pages/MainPage';
 
 function App() {
   return (
-    <div className="App">
-      <SearchBar></SearchBar>
-    </div>
+    <Routes>
+      <Route path="/" element={<MainPage />}></Route>
+      <Route path="/about" element={<AboutPage />}></Route>
+      <Route path="/*" element={<Page404 />}></Route>
+    </Routes>
   );
 }
 
