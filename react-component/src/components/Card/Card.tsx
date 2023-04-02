6f58fc3087a7aa9a88b8cd1bd01f28d5eb70da8d
sluzerpp <bistboy.es@gmail.com>
2023-04-02 11:01:44 +0300

    feat: make card functional component

--- react-component/src/components/Card/Card.tsx
+++ react-component/src/components/Card/Card.tsx
@@ -1,7 +1,7 @@
 import './Card.css';
 import LikeSvg from '../../components/svg/LikeSvg';
 import { IProjectData } from '../../model/CardData';
-import React, { Component } from 'react';
+import React from 'react';
 import EyeSvg from '../../components/svg/EyeSvg';
 import StarSvg from '../../components/svg/StarSvg';
 
@@ -9,41 +9,40 @@ type CardProps = {
   project: IProjectData;
 };
 
-export default class Card extends Component<CardProps> {
-  render() {
-    const { name, img, creator, views, likes, tags, date, isImportant } = this.props.project;
-    return (
-      <div className="card">
-        <div className="card__img" style={{ backgroundImage: `url(${img})` }}></div>
-        {isImportant && <div className="card__important">Important</div>}
-        <div className="card__content">
-          <a href="#" className="card__title">
-            <h2>{name}</h2>
-          </a>
-          <div className="card__date">{date.toLocaleDateString()}</div>
-          <a href="#" className="card__creator">
-            by <span>{creator}</span>
-          </a>
-        </div>
-        <div className="card__tags">
-          {tags.reduce((acc, cur) => (acc ? `${acc}, ${cur}` : cur), '')}
-        </div>
-        <div className="card__footer">
-          <div className="card__counters">
-            <button className="card__counter">
-              <LikeSvg />
-              {likes}
-            </button>
-            <div className="card__counter">
-              <EyeSvg />
-              {views}
-            </div>
-          </div>
-          <button className="card__favorite">
-            <StarSvg />
-          </button>
-        </div>
+export default function Card(props: CardProps) {
+  const { name, img, creator, views, likes, tags, date, isImportant } = props.project;
+
+  return (
+    <div className="card">
+      <div className="card__img" style={{ backgroundImage: `url(${img})` }}></div>
+      {isImportant && <div className="card__important">Important</div>}
+      <div className="card__content">
+        <a href="#" className="card__title">
+          <h2>{name}</h2>
+        </a>
+        <div className="card__date">{date.toLocaleDateString()}</div>
+        <a href="#" className="card__creator">
+          by <span>{creator}</span>
+        </a>
       </div>
-    );
-  }
+      <div className="card__tags">
+        {tags.reduce((acc, cur) => (acc ? `${acc}, ${cur}` : cur), '')}
+      </div>
+      <div className="card__footer">
+        <div className="card__counters">
+          <button className="card__counter">
+            <LikeSvg />
+            {likes}
+          </button>
+          <div className="card__counter">
+            <EyeSvg />
+            {views}
+          </div>
+        </div>
+        <button className="card__favorite">
+          <StarSvg />
+        </button>
+      </div>
+    </div>
+  );
 }
