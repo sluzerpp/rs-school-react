ee2d1300abe2e4858f49c6cb2e047b049a909dfd
sluzerpp <bistboy.es@gmail.com>
2023-04-02 13:19:29 +0300

    feat: make forms functional component

--- react-component/src/pages/Forms.tsx
+++ react-component/src/pages/Forms.tsx
@@ -1,46 +1,33 @@
 import CardForm, { CardFormData } from '../components/CardForm/CardForm';
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { IProjectData } from 'model/CardData';
 import CardList from '../components/CardList/CardList';
 
-type IFormsState = {
-  cards: IProjectData[];
-  currentId: number;
-};
+export default function Forms() {
+  const [cards, setCards] = useState<IProjectData[]>([]);
+  const [currentId, setCurrentId] = useState(0);
 
-export default class Forms extends Component<object, IFormsState> {
-  state = {
-    cards: [],
-    currentId: 0,
-  };
-
-  constructor(props: object) {
-    super(props);
-    this.addCard = this.addCard.bind(this);
-  }
-
-  addCard(data: CardFormData) {
+  const addCard = (data: CardFormData) => {
     const url = URL.createObjectURL(data.img[0]);
-    this.setState((prev) => {
-      prev.cards.push({
+    setCards((prev) => {
+      prev.push({
         ...data,
         img: url,
         likes: 0,
         views: 0,
-        id: this.state.currentId,
+        id: currentId,
       });
-      return { cards: prev.cards, currentId: prev.currentId + 1 };
+      setCurrentId((prev) => prev + 1);
+      return prev;
     });
-  }
+  };
 
-  render() {
-    return (
-      <div className="container">
-        <div className="form-page">
-          <CardForm submitCallback={this.addCard} />
-          <CardList projects={this.state.cards}></CardList>
-        </div>
+  return (
+    <div className="container">
+      <div className="form-page">
+        <CardForm submitCallback={addCard} />
+        <CardList projects={cards}></CardList>
       </div>
-    );
-  }
+    </div>
+  );
 }
