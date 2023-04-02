496d8c5e65bbd5e71f61b5ad76f45d56a0a4c9e1
sluzerpp <bistboy.es@gmail.com>
2023-04-02 13:20:17 +0300

    feat: make mainpage functional component

--- react-component/src/pages/MainPage.tsx
+++ react-component/src/pages/MainPage.tsx
@@ -1,19 +1,17 @@
 import SearchBar from '../components/SearchBar/SearchBar';
-import React, { Component } from 'react';
+import React from 'react';
 import { CardData } from '../model/CardData';
 import CardList from '../components/CardList/CardList';
 
-export default class MainPage extends Component {
-  render() {
-    return (
-      <div className="container">
-        <div className="main">
-          <div className="controls">
-            <SearchBar></SearchBar>
-          </div>
-          <CardList projects={CardData}></CardList>
+export default function MainPage() {
+  return (
+    <div className="container">
+      <div className="main">
+        <div className="controls">
+          <SearchBar></SearchBar>
         </div>
+        <CardList projects={CardData}></CardList>
       </div>
-    );
-  }
+    </div>
+  );
 }
